f246f178c404a0020f20c18272f142fca1dde54d
zarram89 <zarram89@gmail.com>
2024-09-06 04:27:00 +0000

    Deploying to gh-pages from @ zarram89/241042-cat-energy-32@6bc14e5cf2a0f2cd64e8f032e2918f6f1bd2cb74 🚀

--- 15/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var n=document.querySelector(".main-nav"),a=document.querySelector(".main-nav__toggle");n.classList.remove("main-nav--nojs");a.addEventListener("click",()=>{n.classList.contains("main-nav--closed")?(n.classList.remove("main-nav--closed"),n.classList.add("main-nav--opened")):(n.classList.add("main-nav--closed"),n.classList.remove("main-nav--opened"))});
